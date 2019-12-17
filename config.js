6129cc83351e7c384ece8b8221178f3e3989916a
Shubham Pandey <shubhampandey1319@gmail.com>
2019-12-17 15:23:15 +0530

    Add exisiting file

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ const env = process.env.NODE_ENV || 'production'
 //insert your API Key & Secret for each environment, keep this file local and never push it to repo.
 const config = {
 	development :{
-		clientID : 'Kg1N3RtDTfCBNDG17OYcw',
-		clientSecret : '66P6OA7lghqwpNc2uGw66K23KfKY4bMO',
-		redirectUrl : 'http://d5ff7e52.ngrok.io'
+		clientID : 'tpyzcD7NRGqUD5vWCIhtVg',
+		clientSecret : 'BezjwwPpIwj2s1fstbO2eoXsr3mwZnVD',
+		redirectUrl : 'https://133f92d2.ngrok.io'
 	},
 	production:{	
 		clientID : '',
